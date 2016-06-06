eb8321d80c94cca7c9a2d2df525a12b7936d7320
Luc <mac@white.local>
2016-06-06 09:11:55 -0700

    Got Loggins working, and reworked Ajax
    Created default debug account to auto login when you press login. Also
    the always sunny photo got taken down so I commented out Image element.
    Reworked Jason parsing to fit with the reworking of the backend.
    Chocolate sauce

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-var top30, numIteratedPerPage=10;
-
+var top30, numIteratedPerPage = 30,
+accountInfo, isLoggedIn = false,
+questionsAPage = 30, totalQueriedQuestions;
+var usr = 'h', pass = 'fuck';
 var agoraApp = angular.module('agoraApp', ['ngRoute', 'ngAnimate', 'ui.bootstrap']);
 
 agoraApp.config(['$routeProvider', function($routeProvider) {
@@ -7,67 +9,137 @@ agoraApp.config(['$routeProvider', function($routeProvider) {
    .when('/questions', {
     templateUrl: 'top30.html',
     controller: 'top30'
-  })
-  .when('/account', {
+  }).when('/account', {
     templateUrl: 'account.html',
-    controller: 'Account'
+    controller: 'accountController'
   }).otherwise({
     redirectTo: '/questions'
   }).when('/about', {
     templateUrl: 'about.html',
     controller: 'About'
+  }).when('/createaccount', {
+    templateUrl: 'createaccount.html',
+    controller: 'createAccountContoller'
+  }).when('/login', {
+    templateUrl: 'login.html',
+    controller: 'loginController'
   }).otherwise({
     redirectTo: '/questions'
   });
 }]);
+agoraApp.controller('accountController', function ($scope, $route, $routeParams) {
+  $scope.name = 'accountController';
+  $scope.$params = $routeParams;
+  $scope.accountInfoTemp;
+  $scope.refresh = function() {
+      $scope.accountInfoTemp = accountInfo;
+      console.log($scope.accountInfoTemp.id);
+    }
+});
+agoraApp.controller('loginController', function ($scope, $route,
+                                $routeParams, $location){
+  $scope.name = 'loginController';
+  $scope.$params = $routeParams;
+
+});
+agoraApp.controller('loginForm', function ($scope, $http){
+  $scope.name = 'loginForm';
+  $scope.username = '';
+  $scope.password = '';
+  $scope.accountInfoTemp = [];
+  $scope.submit = function() {
+    accountInfo = '';
+    $http({
+     method: 'GET',
+     url: 'https://startandselect.com/scripts/Login.php',
+    //  params: {username: $scope.username,
+    //           password: $scope.password}
+    //debug default account
+    params: {username: usr,
+             password: pass}
+   }).then(function successCallback(response) {
+       // this callback will be called asynchronously
+       // when the response is available
+      //  console.log(response);
+       $scope.accountInfoTemp = angular.toJson(response.data);
+       accountInfo = $scope.accountInfoTemp;
+       isLoggedIn = true;
+       console.log(accountInfo);
+     }, function errorCallback(response) {
+       // called asynchronously if an error occurs
+       // or server returns response with an error status.
+
+       $scope.tempResponse = JSON.parse(JSON.stringify(response.data));
+       accountInfo = $scope.tempResponse;
+       console.log('response');
+     });
+  }
+
+});
+agoraApp.controller('createAccountContoller', function ($scope, $http, $route,
+                                $routeParams, $location){
+  $scope.name = 'createAccountContoller';
+  $scope.$params = $routeParams;
+
+});
+agoraApp.controller('pager', function ($scope){
+  $scope.name = 'pager';
+  $scope.totalItems = totalQueriedQuestions;
+  $scope.currentPage = questionsAPage;
+
+});
 agoraApp.controller('top30', function ($scope, $http, $route,
                                 $routeParams, $location) {
 // routing goodies
   $scope.name = 'top30';
   $scope.$params = $routeParams;
 //manage questions
-  var pageNum = 0;
+  $scope.pageNum = 0;
+  $scope.totalQuestions;
+  $scope.totalPages;
+  //search term
+  $scope.currentSearchTerm = '';
   //get request questions
   function getTopQuestions(pgN) {
     //multiply page number for first question desired
     var startQuestion = pgN*numIteratedPerPage;
     //request
     $http({
-     method: 'GET',
+     method: 'POST',
+     //Deprecated - url: 'http://startandselect.com/scripts/GetPopularQuestion.php',
      url: 'http://startandselect.com/scripts/GetPopularQuestion.php',
+    //  url: 'https://startandselect.com/scripts/GetPopularQuestion.php',
      params: {offset: startQuestion}
    }).then(function successCallback(response) {
        // this callback will be called asynchronously
        // when the response is available
-       top30 = JSON.parse(JSON.stringify(response.data));
+       top30 = JSON.parse(JSON.stringify(response.data.questions));
        $scope.questions = top30;
+       $scope.totalQuestions = response.data.length;
+       totalQueriedQuestions = $scope.totalQuestions;
+       $scope.totalPages = $scope.totalQuestions / questionsAPage;
+       console.log(response);
      }, function errorCallback(response) {
        // called asynchronously if an error occurs
        // or server returns response with an error status.
-       console.log('outta questions diggy dog');
+       console.log("TF");
      });
   };
   //initial call
-  getTopQuestions(pageNum);
+  getTopQuestions($scope.pageNum);
   //go forward a page
   $scope.nextPage = function() {
-    pageNum++;
-    getTopQuestions(pageNum);
-    $scope.$apply();
+    $scope.pageNum++;
+    getTopQuestions($scope.pageNum);
   };
   //go back a page
   $scope.backPage = function() {
-    if(pageNum>0)
-      pageNum--;
-    getTopQuestions(pageNum);
-    $scope.$apply();
+    if($scope.pageNum>0)
+      $scope.pageNum--;
+    getTopQuestions($scope.pageNum);
   };
   //get current page num
   $scope.getPageNum = function() {
-    return pageNum;
+    return $scope.pageNum;
   };
-});
-agoraApp.controller('Account', function ($scope, $http) {
-
-
 });
