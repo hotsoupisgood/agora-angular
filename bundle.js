bb72582fd17d089f793d5e70f901091af6041945
Luc <mac@Applesauce.local>
2017-06-05 01:53:33 -0700

    impoved add comment look

--- bundle.js
+++ bundle.js
@@ -81,7 +81,6 @@ module.exports =  function($scope) {
 module.exports = function($scope, submitCommentService) {
     $scope.comments = {};
     $scope.commentOpen = true;
-    $scope.commentText=''
     $scope.getCommentsFull = function () {
       getSingleQuestionService.get($scope.section.id).then(function (response) {
         $scope.question = response;
@@ -92,7 +91,6 @@ module.exports = function($scope, submitCommentService) {
       $scope.loading = true;
       submitCommentService.submit($scope.section.id, $scope.commentText)
       .then(function (response) {
-        console.log('fuck');
         if (response) {
           $scope.success = response;
           $scope.loading = false;
